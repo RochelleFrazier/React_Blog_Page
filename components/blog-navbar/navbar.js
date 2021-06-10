7714c1277f471856fff787b68271ddacb72a7926
Rochelle Frazier <rochellesakura@gmail.com>
2021-06-10 03:53:20 +0000

    Updated index.js and added css.

--- components/blog-navbar/navbar.js
+++ components/blog-navbar/navbar.js
@@ -10,14 +10,14 @@ const Navbar = () => {
       }
     >
       <a className={"navbar-brand"} href="#">
-       <!-- <img className={styles.img} src="#" alt="Real Estate logo" /> -->
+        {/* <img className={styles.img} src="#" alt="Real Estate logo" /> */}
         <a className={styles.logoname + " ml-1 mb-1"}>The Real Estate Blog</a>
       </a>
 
       <ul className={"navbar-nav ml-auto"}>
         <li className={styles.link + " nav-item"}>
           <a className={"nav-link"} href="#">
-            Visit Ezcaza.com <span class="sr-only">(current)</span>
+            Visit TheRealEstate.com <span class="sr-only">(current)</span>
             <i class="fa fa-arrow-right"></i>
           </a>
         </li>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from "next/head";
-import styles from "../styles/blog.module.css";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Ezcaza University</title>
+        <title>The Real Estate Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <h1>Welcome to EZCAZA University!</h1>
+      <div className={styles.title}>
+        <h1>Welcome to The Real Estate Blog!</h1>
+        <p>Builted by Rochelle Frazier</p>
+      </div>
+      <div className={styles.blogpage}>
+          <p>/blog to redirect to blog page.</p>
       </div>
     </div>
   );
